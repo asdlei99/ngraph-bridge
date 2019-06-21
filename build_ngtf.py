10da971a9ca436a1953c158998c6d11c709168a0
Kam D Kasravi <kam.d.kasravi@intel.com>
2019-06-21 10:40:18 -0700

    add start_container, use default TEST_TMPDIR in container (/root/.cache/bazel)

--- build_ngtf.py
+++ build_ngtf.py
@@ -153,6 +153,11 @@ def main():
         action="store_true")
 
     parser.add_argument(
+        '--start_container',
+        help="Starts the docker container\n",
+        action="store_true")
+
+    parser.add_argument(
         '--stop_container',
         help="Stops the docker container\n",
         action="store_true")
@@ -169,6 +174,10 @@ def main():
         build_base(arguments)
         return
 
+    if arguments.start_container:
+        start_container("/ngtf")
+        return
+
     if arguments.stop_container:
         stop_container()
         return
@@ -176,8 +185,8 @@ def main():
     if arguments.run_in_docker:
         if check_container() == True:
             stop_container()
-        start_container("/ngtf", ".cache/ngtf")
-        run_in_docker("/ngtf/build_ngtf.py", ".cache/ngtf", arguments)
+        start_container("/ngtf")
+        run_in_docker("/ngtf/build_ngtf.py", arguments)
         return
 
     if (arguments.debug_build):
